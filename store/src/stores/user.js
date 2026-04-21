a7a29e7f1f1dd6ec82f3ed5fd710e5eb658e84af
Ben Chen <idben.chen@gmail.com>
2026-04-21 14:57:49 +0800

    實際接API

--- store/src/stores/user.js
+++ store/src/stores/user.js
@@ -5,10 +5,19 @@ export const useUserStore = defineStore("user", () => {
     const user = ref(null)
     const isLogin = computed(() => user.value);
 
-    const myLogin = function () {
+    const myLogin = async function () {
+        // user.value = {
+        //     name: "Lucas Carr",
+        //     img: "https://randomuser.me/api/portraits/men/68.jpg"
+        // }
+        const url = "https://randomuser.me/api";
+        // fetch().then().then().catch()
+        const response = await fetch(url, { method: "GET" })
+        const result = await response.json();
+        console.log(result);
         user.value = {
-            name: "Lucas Carr",
-            img: "https://randomuser.me/api/portraits/men/68.jpg"
+            name: result.results[0].first,
+            img: result.results[0].picture.large
         }
     }
 
--- store/src/views/Test02View.vue
+++ store/src/views/Test02View.vue
@@ -4,10 +4,12 @@ import { storeToRefs } from 'pinia';
 const userStore = useUserStore();
 
 const { isLogin, user } = storeToRefs(userStore)
+const { login, logout } = userStore;
 
 </script>
 <template>
-    <button v-if="!isLogin" class="btn btn-primary">登入</button>
-    <button v-else class="btn btn-danger">登出</button>
+    <img :src="user && user.img" alt="">
+    <button @click="login()" v-if="!isLogin" class="btn btn-primary">登入</button>
+    <button @click="logout()" v-else class="btn btn-danger">登出</button>
 </template>
 <style scoped></style>
