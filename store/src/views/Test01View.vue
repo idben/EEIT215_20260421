01b57b0d4c1a270effb64e657979bd52fb0df7ca
Ben Chen <idben.chen@gmail.com>
2026-04-21 11:42:07 +0800

    99+

--- store/src/views/Test01View.vue
+++ store/src/views/Test01View.vue
@@ -10,7 +10,13 @@ const cartStore = useCartStore();
         <p>是否為空：{{ cartStore.isEmpty }}</p>
 
         <button class="btn btn-primary me-1">加入商品</button>
-        <button class="btn btn-primary">清空</button>
+        <button class="btn btn-primary position-relative">
+            清空
+            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                {{ cartStore.totalItems }}
+                <span class="visually-hidden">unread messages</span>
+            </span>
+        </button>
 
         <ul>
             <li v-for="item in cartStore.items">
