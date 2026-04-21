256142f496b1b801bec90d1b860862c909f4db9f
Ben Chen <idben.chen@gmail.com>
2026-04-21 15:06:31 +0800

    登入資料的顥示

--- store/src/stores/user.js
+++ store/src/stores/user.js
@@ -6,17 +6,19 @@ export const useUserStore = defineStore("user", () => {
     const isLogin = computed(() => user.value);
 
     const myLogin = async function () {
+        // 第一次: 設計的假資料
         // user.value = {
         //     name: "Lucas Carr",
         //     img: "https://randomuser.me/api/portraits/men/68.jpg"
         // }
+        // 第二次: 從 randomuser 網站的 api 實際介接
         const url = "https://randomuser.me/api";
         // fetch().then().then().catch()
         const response = await fetch(url, { method: "GET" })
         const result = await response.json();
         console.log(result);
         user.value = {
-            name: result.results[0].first,
+            name: result.results[0].name.first + " " + result.results[0].name.last,
             img: result.results[0].picture.large
         }
     }
--- store/src/views/Test02View.vue
+++ store/src/views/Test02View.vue
@@ -8,7 +8,11 @@ const { login, logout } = userStore;
 
 </script>
 <template>
-    <img :src="user && user.img" alt="">
+    <div class="d-flex align-items-center">
+        <img class="rounded-circle" :src="user && user.img" alt="">
+        <h1>{{ user && user.name }}</h1>
+    </div>
+
     <button @click="login()" v-if="!isLogin" class="btn btn-primary">登入</button>
     <button @click="logout()" v-else class="btn btn-danger">登出</button>
 </template>
